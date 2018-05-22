32fa88e1cbcb40d76591e1642d02f607b3222a25
superpuper32 <pochitalin_m@mail.ru>
2018-05-22 20:29:00 +0300

    root-level file server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const errorhandler = require('errorhandler');
+
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+const apiRouter = require('./api/api');
+app.use('/api', apiRouter);
+
+//  Body parsing middleware
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.use(errorhandler());
+
+module.exports = app;
